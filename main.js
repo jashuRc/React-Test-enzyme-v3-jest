84abffbcf2fb51290ee1fa49493d65105296feaa
Jashu <jashuchaudhary99@gmail.com>
2018-01-11 17:46:12 +0530

    .babelrc added .js files added

---
+++ main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CheckboxWithLabel from './src/components/CheckboxWithLabel'
+
+ReactDOM.render(
+    <CheckboxWithLabel />
+    ,document.getElementById('main')
+)
--- src/components/CheckboxWithLabel.js
+++ src/components/CheckboxWithLabel.js
@@ -1,5 +1,3 @@
-// CheckboxWithLabel.js
-
 import React from 'react';
 
 export default class CheckboxWithLabel extends React.Component {
@@ -7,9 +5,6 @@ export default class CheckboxWithLabel extends React.Component {
     constructor(props) {
         super(props);
         this.state = {isChecked: false};
-
-        // bind manually because React class components don't auto-bind
-        // http://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#autobinding
         this.onChange = this.onChange.bind(this);
     }
 
